d6c60725287b6203ab4f2249e3fe7b2ac2270ea8
qrames <ramque@orange.fr>
2018-10-17 14:59:14 +0200

    date diff work not (h:i:s)

--- header-launching.php
+++ header-launching.php
@@ -17,13 +17,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri()?>/node_modules/bootstrap/dist/css/bootstrap.min.css">
-
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 	<header>
-		caca prout
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
--- template-launching.php
+++ template-launching.php
@@ -4,46 +4,72 @@ Template Name: launching
 Template Post Type: post, page, product
 */
 
-      
+
 
 ?>
 
 <body>
-		
-		
+
+
 	<?php get_header("launching"); ?>
 
 
 	<?php
-	$id=23;
+	$id=get_the_id();
+
 	$date_release=get_post_meta($id, 'date_release')[0];
-	$day_release=get_post_meta($id, 'date_release')[1];
-	$title=get_post_meta($id, 'Title')[0];
-	
+    $dtz = new DateTimeZone("Europe/Paris"); //Your timezone
+    $start = new DateTime( 'now', new DateTimeZone('Europe/Paris'));
+    var_dump($start);
+    echo $date_release;
+    $end = new DateTime($date_release);
+    $duration = $end->Diff($start);
+
 ?>
 
 	<div class="container-fluid">
 
 		<div id="primary" class="content-area">
 			<main id="main" class="launching site-main">
-
-				<h1> <?php echo $title ?></h1>
-				<!-- <div id="count"> <?php echo $date_release ?> </div> -->
-				<!-- <div id="countlegend"> <?php echo $day_release ?> </div> -->
+                <i class="fab fa-fort-awesome"></i>
+				<h1>WE ARE LAUNCHING IN</h1>
+				<div id="count"> <?php echo $duration->format('%Y %m %d %h:%i:%s'); ?> </div>
 
 				<p id="countdown"></p>
-				
-				
+
+
 				<p class="text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit doloremque non aliquid tempora corporis, dolorum aspernatur incidunt totam esse dolore ad, atque praesentium quia culpa excepturi, facilis alias consequatur asperiores.</p>
 				<p class="text">bla bla codi n camp</p>
-				
+
 				<button class="btn btn-danger" type="submit">Notify me</button>
 				<button type="button" class="btn btn-outline-light">About us</button>
-				
-				
+
+                <ul class="social-network">
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fab fa-google-plus-g"></i>
+                        </a>
+                    </li>
+                </ul>
+
 			</main><!-- #main -->
 		</div><!-- #primary -->
-		
+
 	</div>
 <?php
 get_footer("launching");
